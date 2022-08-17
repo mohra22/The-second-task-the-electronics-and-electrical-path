b2cde53f1e8786736994b6df36eaaca04d0991cf
mohrah Aljuaid <muhrah21@gmail.com>
2022-08-18 01:03:57 +0300

    Add files via upload

---
+++ Untitled2_TMP/Untitled2_TMP.ino
@@ -0,0 +1,15 @@
+#define LED 13
+#define BUTTON 7
+int va l= 0;
+void setup() {
+pinMode(LED,OUTPUT);
+pinMode(BUTTON,INPUT);
+}
+void loop(){
+val = digitalRead(BUTTON);
+if(val==HIGH){
+digitalWrite(LED,HIGH);
+}else{
+digitalWrite(LED,LOW);
+}
+}
